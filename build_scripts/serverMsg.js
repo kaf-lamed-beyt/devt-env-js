e58a7afa4c2d534ba97ab6554bea2343d09115ee
Caleb335 <belac335@gmail.com>
2020-06-20 20:50:52 +0100

    add server location

--- build_scripts/serverMsg.js
+++ build_scripts/serverMsg.js
@@ -1,4 +1,7 @@
 const chalk = require('chalk')
 
 const message = chalk.green('Starting server in dev mode...')
+// the variable below shows the location of the devolpment server in your browser on the terminal
+const devAddress = chalk.green('Server is running on localhost:1234')
 console.log(message)
+console.log(devAddress)
